3570e79e2de63dd262ce9fb500d0d9fc83746c01
Denise Andrea Name Ramirez <dnameramirez@gmail.com>
2025-10-29 09:19:00 +0100

    publishing code
    heheheha

---
+++ bad-coding/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+        startCode();
+    }
+
+    public void startCode(){
+        while (true){
+            String idk = randomCode();
+            String experiment = randomWord
+
+            System.out.println("lol " + idk);
+        }
+    }
+
+    public String randomCode(){
+        Random random = new Random();
+        int randomizer = random.nextInt(3);
+
+        if (randomizer == 0) return "hola";
+        if (randomizer == 1) return "hi"
+        return "hello";
+    }
+
+    public String randomWord {
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+        String literallyNothing;
+
+        switch (choice) {
+            case 1;
+                System.out.println("aisdhoa");
+                System.out.println(literallyNothing + " tf");
+            case 2:
+                System.out.println("mskadiwo");
+            case 3:
+                System.out.println("suaodyhwuoia");
+                System.out.println(literallyNothing + " wtf");
+            case 4:
+                System.out.println("sioada");
+            case 5:
+                System.out.println("siofjanwka");
+    }
+        // no se como hacer algo simple no entendible ayuda
+}
